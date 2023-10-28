1bdc1b7cf04bce96dfb75544413df1392adc42e2
Manav Gagvani <mgagvani@outlook.com>
2023-10-28 12:19:01 -0400

    another try

--- rl_test.py
+++ rl_test.py
@@ -16,26 +16,32 @@ conf = {
         "racer_name": "Manav Gagvani",
         "country": "USA",
         "bio": ":)",
-        "max_cte": 10,
+        "max_cte": 7.5,
+
+        "throttle_min": 0.0,
     }    
 
-timesteps = 10_000
+timesteps = 1_000_000
 
 if __name__ == "__main__":
     env = gym.make("donkey-mountain-track-v0", conf=conf)
 
     # Train the agent
-    # model = PPO("CnnPolicy", env, verbose=1)
+    model = PPO("CnnPolicy", env, verbose=1)
 
     # Load most recent checkpoint
-    model_path = max(glob.glob('./logs/*.zip'), key=(timestamp:=os.path.getctime))
-    model = PPO.load(model_path, env=env, verbose=1)
+    model_path = max(glob.glob('./logs/*.zip'), key=os.path.getctime)
+    # model = PPO.load(model_path, env=env, verbose=1)
 
-    # TODO
-    # print("Loaded model from: ", model_path, " saved at: ", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp)))
+    # find date and time of saved model
+    save_time = os.path.getctime(model_path)
+    # convert to human readable time
+    save_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(save_time))
+    assert type(save_time) == str, type(save_time)
+    print("Loaded model from", model_path, "saved at:", save_time)
     
     # callback to save model
-    trained_model = model.learn(total_timesteps=timesteps, progress_bar=True, callback=CheckpointCallback(save_freq=500, save_path='./logs/'))
+    trained_model = model.learn(total_timesteps=timesteps, progress_bar=True, callback=CheckpointCallback(save_freq=15000, save_path='./logs/'))
 
     # Save the agent
     # trained_model.save("ppo_donkey")
